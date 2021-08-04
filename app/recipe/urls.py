4970b63792a82097b870cda5730b2a9e90fc4302
Dario Acevedo <darioacevedo@MacBook-Pro-de-Dario.local>
2021-08-03 19:59:25 -0500

    Added recipes model and also list recipes apiÃ

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -2,12 +2,13 @@ from django.urls import path, include
 from rest_framework import routers, views
 from rest_framework.routers import DefaultRouter
 
-from .views import TagViewSet, IngredientViewSet
+from .views import RecipeViewSet, TagViewSet, IngredientViewSet
 
 
 router = DefaultRouter()
 router.register('tags', TagViewSet)
 router.register('ingredients', IngredientViewSet)
+router.register('recipes', RecipeViewSet)
 
 app_name = 'recipe'
 

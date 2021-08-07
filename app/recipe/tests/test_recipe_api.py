5163a0450b990f0876843389e3d7577ca66325fc
Dario Acevedo <dario.acevedo@cornershopapp.com>
2021-08-06 21:28:14 -0500

    tests for update recipe added, modified travis.yml

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -161,3 +161,41 @@ class PrivateRecipeApiTest(TestCase):
         self.assertEqual(ingredients.count(),2)
         self.assertIn(ing1, ingredients)
         self.assertIn(ing2, ingredients)
+
+    def test_partial_update_recipe(self):
+        """Test updating a recipe with patch"""
+        recipe = sample_recipe(user=self.user)
+        recipe.tags.add(sample_tag(user=self.user, name='Curry'))
+        new_tag = sample_tag(user=self.user, name='Chicken')
+
+        payload = {'title':'Chicken ticka', 'tags': [new_tag.id]}
+        url = detail_url(recipe.id)
+        self.client.patch(url, payload)
+
+        recipe.refresh_from_db()
+        self.assertEqual(recipe.title, payload['title'])
+        
+        tags = recipe.tags.all()
+        self.assertEqual(len(tags),1)
+
+        self.assertIn(new_tag, tags)
+
+
+    def test_full_update_recipe(self):
+
+        recipe = sample_recipe(user=self.user)
+        recipe.tags.add(sample_tag(user=self.user, name='Default'))
+        payload = {
+            'title': 'Spaghetti carbonara',
+            'time_minutes': 25,
+            'price': 5.00
+        }
+        url = detail_url(recipe.id)
+        self.client.put(url, payload)
+        recipe.refresh_from_db()
+        self.assertEqual(recipe.title, payload['title'])
+        
+        self.assertEqual(recipe.time_minutes, payload['time_minutes'])
+        self.assertEqual(recipe.price, payload['price'])
+        tags = recipe.tags.all()
+        self.assertEqual(len(tags), 0)
